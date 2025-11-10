10054c3411501796044a432f0d681dcc9be8db7f
Artem Gardysh <artemgardysh@gmail.com>
2025-11-10 18:49:16 +0000

    updated task 3

--- js/task-3.js
+++ js/task-3.js
@@ -4,9 +4,6 @@ const outputEl = document.querySelector("span#name-output");
 
 const outputTextContent = inputEl.addEventListener("input", (event) => {
   outputEl.textContent = event.currentTarget.value.trim();
-});
-
-inputEl.addEventListener("blur", () => {
   if (!inputEl.value || inputEl.value.trim() === "") {
     return (outputEl.textContent = "Anonymous");
   }
