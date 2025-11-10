c090848020d9dbeb01bf49b032287b10eb754c05
Artem Gardysh <artemgardysh@gmail.com>
2025-11-10 18:56:02 +0000

    updated task 3

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,6 @@ const outputEl = document.querySelector("span#name-output");
 
 const outputTextContent = inputEl.addEventListener("input", (event) => {
   const trimmedValue = inputEl.value.trim();
-  outputEl.textContent = "Anon";
 
   if (trimmedValue === "") {
     outputEl.textContent = "Anonymous";
